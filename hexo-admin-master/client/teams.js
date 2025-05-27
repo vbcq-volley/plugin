3c22bd4a84df865c17a212ef3f7cd189e88ef943
alphaleadership <thomas.iniguez@free.fr>
2025-05-27 13:27:35 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/teams.js
+++ hexo-admin-master/client/teams.js
@@ -78,7 +78,7 @@ var Datas = React.createClass({
     var current = this.state.pages[this.state.selected] || {}
     var url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/')
     var rootPath = url.slice(0, url.indexOf('admin')).join('/')
-    return <div className="posts">
+    return <div className="teams-container">
       <div className="posts_header">
         <h2>Équipes</h2>
         <button onClick={this.toggleNewForm} className="new-team-button">
@@ -90,36 +90,38 @@ var Datas = React.createClass({
           <Newteam onNew={this._onNew}/>
         </div>
       )}
-      <ul className='posts_list'>
-        {
-          this.state.pages.map((page, i) =>
-            <li key={page._id} className={cx({
-                "posts_post": false,
-                "posts_post--draft": page.isDraft,
-                "posts_post--selected": i === this.state.selected
-              })}
-              onDoubleClick={this.goTo.bind(null, page._id)}
-              onClick={this.setState.bind(this, {selected: i}, null)}
-            >
-              <span className="posts_post-title">
-                {page.teamName}
-              </span>
-              <span className="posts_post-date">
-                {moment(page.date).format('MMM Do YYYY')}
-              </span>
-              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + page.path}>
-                <i className='fa fa-link'/>
-              </a>
-              <Link className='posts_edit-link' to="data" matchId={page._id}>
-                <i className='fa fa-pencil'/>
-              </Link>
-              <a className='posts_delete-link' onClick={this._onDelete.bind(null, page.index)}>
-                <i className='fa fa-trash'/>
-              </a>
-            </li>
-          )
-        }
-      </ul>
+      <div className="teams-list">
+        <ul className='posts_list'>
+          {
+            this.state.pages.map((page, i) =>
+              <li key={page._id} className={cx({
+                  "posts_post": false,
+                  "posts_post--draft": page.isDraft,
+                  "posts_post--selected": i === this.state.selected
+                })}
+                onDoubleClick={this.goTo.bind(null, page._id)}
+                onClick={this.setState.bind(this, {selected: i}, null)}
+              >
+                <span className="posts_post-title">
+                  {page.teamName}
+                </span>
+                <span className="posts_post-date">
+                  {moment(page.date).format('MMM Do YYYY')}
+                </span>
+                <a className='posts_perma-link' target="_blank" href={rootPath + '/' + page.path}>
+                  <i className='fa fa-link'/>
+                </a>
+                <Link className='posts_edit-link' to="data" matchId={page._id}>
+                  <i className='fa fa-pencil'/>
+                </Link>
+                <a className='posts_delete-link' onClick={this._onDelete.bind(null, page.index)}>
+                  <i className='fa fa-trash'/>
+                </a>
+              </li>
+            )
+          }
+        </ul>
+      </div>
       <div className={cx({
         'posts_display': true,
         'posts_display--draft': current.isDraft
