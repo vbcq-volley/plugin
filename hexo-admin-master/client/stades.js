d99fd1495903be70c909f6f8b130f152836e8f43
alphaleadership <thomas.iniguez@free.fr>
2025-05-27 12:49:19 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/stades.js
+++ hexo-admin-master/client/stades.js
@@ -1,123 +1,114 @@
 var React = require('react/addons')
-var Router = require('react-router')
-var Link = Router.Link
-var api = require('./api')
-var NewStade = require('./new-stade')
+var cx = React.addons.classSet
+var Link = require('react-router').Link;
+var Router = require('react-router');
+var _ = require('lodash')
+var moment = require('moment')
+var SinceWhen = require('./since-when')
 
+var Rendered = require('./rendered')
+var DataFetcher = require('./data-fetcher');
+var NewStade = require('./new-stade')
+var api = require('./api');
+ 
 var Stades = React.createClass({
-  getInitialState: function() {
+  mixins: [DataFetcher((params) => {
     return {
-      stades: [],
-      showNewForm: false,
-      selectedStade: null
+     
+    }
+  })],
+
+  getInitialState: function () {
+    return {
+      selected: 0,
+      pages: [] // Initialisation à null pour indiquer que les données ne sont pas encore chargées
     }
   },
 
-  componentDidMount: function() {
-    api.getEntries('stade').then((stades) => {
-      this.setState({ stades: stades });
-      this.render();
-    });
+  componentDidMount: function () {
+    api.getEntries("stade").then((data) => {
+      this.setState({pages: data})
+    })
   },
 
-  toggleNewForm: function() {
-    this.setState({ showNewForm: !this.state.showNewForm });
+  _onNew: function (page) {
+    var pages = this.state.pages.slice()
+    pages.unshift(page)
+    this.setState({pages: pages})
+    Router.transitionTo('stade', {matchId: page._id})
   },
 
-  handleStadeSelect: function(stade) {
-    this.setState({ selectedStade: stade });
-  },
-
-  handleDelete: function(id, e) {
+  _onDelete: function (id, e) {
     if (e) {
-      e.preventDefault();
+      e.preventDefault()
     }
     if (confirm('Êtes-vous sûr de vouloir supprimer ce stade ?')) {
       api.deleteEntry("stade", id).then(() => {
-        var stades = this.state.stades.filter(stade => stade._id !== id);
-        this.setState({ stades: stades });
-      });
+        var pages = this.state.pages.filter(page => page._id !== id)
+        this.setState({pages: pages})
+      })
     }
   },
 
-  handleUpdate: function(id, e) {
+  goTo: function (id, e) {
     if (e) {
-      e.preventDefault();
+      e.preventDefault()
     }
-    Router.transitionTo('stade', {stadeId: id});
+    Router.transitionTo('stade', {matchId: id})
   },
 
-  render: function() {
-    return <div className="stades">
-      <div className="stades_header">
-        <h2>Stades</h2>
-        <button onClick={this.toggleNewForm} className="new-stade-button">
-          <i className="fa fa-plus" /> {this.state.showNewForm ? 'Annuler' : 'Nouveau stade'}
-        </button>
-      </div>
-      {this.state.showNewForm && (
-        <div className="new-stade-form-container">
-          <NewStade />
-        </div>
-      )}
-      <div className="stades-container">
-        <div className="stades_list">
-          <table className="stades_table">
-            <thead>
-              <tr>
-                <th>Nom du stade</th>
-                <th>Adresse</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.stades.map((stade) => (
-                <tr key={stade.index}>
-                  <td>{stade.stadeName}</td>
-                  <td>{stade.address}</td>
-                  <td>
-                    <button 
-                      onClick={() => this.handleStadeSelect(stade)}
-                      className="btn btn-info"
-                    >
-                      <i className="fa fa-info-circle" /> Détails
-                    </button>
-                    <Link to={`/stade/${stade._id}`} className="btn btn-primary">
-                      <i className="fa fa-pencil" /> Modifier
-                    </Link>
-                    <button 
-                      onClick={(e) => this.handleDelete(stade._id, e)}
-                      className="btn btn-danger"
-                    >
-                      <i className="fa fa-trash" /> Supprimer
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        
-        {this.state.selectedStade && (
-          <div className="stade-details">
-            <h3>{this.state.selectedStade.stadeName}</h3>
-            <p><strong>Adresse:</strong> {this.state.selectedStade.address}</p>
-            <div className="map-container">
-              <iframe
-                width="100%"
-                height="300"
-                frameBorder="0"
-                scrolling="no"
-                marginHeight="0"
-                marginWidth="0"
-                src={`https://www.openstreetmap.org/export/embed.html?bbox=${encodeURIComponent(this.state.selectedStade.address)}&marker=${encodeURIComponent(this.state.selectedStade.address)}&layer=mapnik`}
-              />
-            </div>
-          </div>
-        )}
+  render: function () {
+    if (!this.state.pages) {
+      return <div className='pages'>Loading...</div>
+    }
+    var current = this.state.pages[this.state.selected] || {}
+    var url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/')
+    var rootPath = url.slice(0, url.indexOf('admin')).join('/')
+    return <div className="posts">
+      <ul className='posts_list'>
+        <NewStade onNew={this._onNew}/>
+        {
+          this.state.pages.map((page, i) =>
+            <li key={page._id} className={cx({
+                "posts_post": false,
+                "posts_post--draft": page.isDraft,
+                "posts_post--selected": i === this.state.selected
+              })}
+              onDoubleClick={this.goTo.bind(null, page._id)}
+              onClick={this.setState.bind(this, {selected: i}, null)}
+            >
+              <span className="posts_post-title">
+                {page.stadeName}
+              </span>
+              <span className="posts_post-date">
+                {moment(page.date).format('MMM Do YYYY')}
+              </span>
+              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + page.path}>
+                <i className='fa fa-link'/>
+              </a>
+              <Link className='posts_edit-link' to="data" matchId={page._id}>
+                <i className='fa fa-pencil'/>
+              </Link>
+              <a className='posts_delete-link' onClick={this._onDelete.bind(null, page._id)}>
+                <i className='fa fa-trash'/>
+              </a>
+            </li>
+          )
+        }
+      </ul>
+      <div className={cx({
+        'posts_display': true,
+        'posts_display--draft': current.isDraft
+      })}>
+        {current.isDraft && <div className="posts_draft-message">Draft</div>}
+        <Rendered
+          ref="rendered"
+          className="posts_content"
+          text={JSON.stringify(current,null,2)}
+          type="stade"/>
       </div>
     </div>
   }
-})
+});
 
-module.exports = Stades
+module.exports = Stades;
