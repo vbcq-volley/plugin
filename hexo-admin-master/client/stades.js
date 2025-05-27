86c2676f0a7d433842f8641d5f975872e7ebb05b
alphaleadership <thomas.iniguez@free.fr>
2025-05-27 14:55:28 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/stades.js
+++ hexo-admin-master/client/stades.js
@@ -30,6 +30,7 @@ var Stades = React.createClass({
 
   componentDidMount: function() {
     api.getEntries("stade").then((stades) => {
+      
       this.setState({stades: stades})
       this.componentDidUpdate()
     })
