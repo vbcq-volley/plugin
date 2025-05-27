4f04ce867cafbe169495efc70f82aac96eba24de
alphaleadership <thomas.iniguez@free.fr>
2025-05-27 13:41:40 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/teams.js
+++ hexo-admin-master/client/teams.js
@@ -10,58 +10,27 @@ var Rendered = require('./rendered')
 var DataFetcher = require('./data-fetcher');
 var Newteam = require('./new-team')
 var api = require('./api');
- 
-var Datas = React.createClass({
+
+var Teams = React.createClass({
   mixins: [DataFetcher((params) => {
     return {
-     
+      teams: api.getEntries("team").then((teams) =>
+        _.sortBy(teams, ['date']).reverse()
+      )
     }
   })],
 
   getInitialState: function () {
     return {
-      selected: 0,
-      pages: [], // Initialisation à null pour indiquer que les données ne sont pas encore chargées
-      showNewForm: false
+      selected: 0
     }
   },
 
-  componentDidMount: function () {
-    api.getEntries("team").then((data) => {
-      this.setState({pages: data})
- 
-    })
-  },
-
-  toggleNewForm: function() {
-    this.setState({ showNewForm: !this.state.showNewForm });
-  },
-
-  _onNew: function (page) {
-    var pages = this.state.pages.slice()
-    console.log(pages)
-    pages.unshift(page)
-    this.setState({pages: pages})
-    Router.transitionTo('team', {matchId: page._id})
-  },
-
-  _onDelete: function (id, e) {
-    if (e) {
-      e.preventDefault()
-    }
-    if (confirm('Êtes-vous sûr de vouloir supprimer cette équipe ?')) {
-      api.deleteEntry("team", id).then(() => {
-        var pages = this.state.pages.filter(page => page.index !== id)
-        this.setState({pages: pages})
-      })
-    }
-  },
-
-  _onUpdate: function (id, e) {
-    if (e) {
-      e.preventDefault()
-    }
-    Router.transitionTo('team', {matchId: id})
+  _onNew: function (team) {
+    var teams = this.state.teams.slice()
+    teams.unshift(team)
+    this.setState({teams: teams})
+    Router.transitionTo('team', {matchId: team._id})
   },
 
   goTo: function (id, e) {
@@ -72,50 +41,37 @@ var Datas = React.createClass({
   },
 
   render: function () {
-    if (!this.state.pages) {
-      return <div className='pages'>Loading...</div>
+    if (!this.state.teams) {
+      return <div className='teams'>Loading...</div>
     }
-    var current = this.state.pages[this.state.selected] || {}
+    var current = this.state.teams[this.state.selected] || {}
     var url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/')
     var rootPath = url.slice(0, url.indexOf('admin')).join('/')
     return <div className="posts">
-      <div className="posts_header">
-        <h2>Équipes</h2>
-        <button onClick={this.toggleNewForm} className="new-team-button">
-          <i className="fa fa-plus" /> {this.state.showNewForm ? 'Annuler' : 'Nouvelle équipe'}
-        </button>
-      </div>
-      {this.state.showNewForm && (
-        <div className="new-team-form-container">
-          <Newteam onNew={this._onNew}/>
-        </div>
-      )}
       <ul className='posts_list'>
+        <Newteam onNew={this._onNew}/>
         {
-          this.state.pages.map((page, i) =>
-            <li key={page._id} className={cx({
-                "posts_post": false,
-                "posts_post--draft": page.isDraft,
+          this.state.teams.map((team, i) =>
+            <li key={team._id} className={cx({
+                "posts_post": true,
+                "posts_post--draft": team.isDraft,
                 "posts_post--selected": i === this.state.selected
               })}
-              onDoubleClick={this.goTo.bind(null, page._id)}
+              onDoubleClick={this.goTo.bind(null, team._id)}
               onClick={this.setState.bind(this, {selected: i}, null)}
             >
               <span className="posts_post-title">
-                {page.teamName}
+                {team.teamName}
               </span>
               <span className="posts_post-date">
-                {moment(page.date).format('MMM Do YYYY')}
+                {moment(team.date).format('MMM Do YYYY')}
               </span>
-              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + page.path}>
+              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + team.path}>
                 <i className='fa fa-link'/>
               </a>
-              <Link className='posts_edit-link' to="data" matchId={page._id}>
+              <Link className='posts_edit-link' to="team" matchId={team._id}>
                 <i className='fa fa-pencil'/>
               </Link>
-              <a className='posts_delete-link' onClick={this._onDelete.bind(null, page.index)}>
-                <i className='fa fa-trash'/>
-              </a>
             </li>
           )
         }
@@ -135,4 +91,4 @@ var Datas = React.createClass({
   }
 });
 
-module.exports = Datas;
+module.exports = Teams;
