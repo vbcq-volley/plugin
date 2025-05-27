f6d477037ee867e98a84e4362b8fbfae19a5e9ea
alphaleadership <thomas.iniguez@free.fr>
2025-05-27 13:42:21 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/results.js
+++ hexo-admin-master/client/results.js
@@ -10,18 +10,19 @@ var Rendered = require('./rendered')
 var DataFetcher = require('./data-fetcher');
 var NewResult = require('./new-result')
 var api = require('./api');
- 
+
 var Results = React.createClass({
   mixins: [DataFetcher((params) => {
     return {
-     
+      results: api.getEntries("result").then((results) =>
+        _.sortBy(results, ['date']).reverse()
+      )
     }
   })],
 
   getInitialState: function () {
     return {
       selected: 0,
-      pages: [], // Initialisation à null pour indiquer que les données ne sont pas encore chargées
       displayKeys: ['text'],
       allKeys: ['team1', 'team1Score', 'team2Score', 'team2', 'matchType', 'isForfeit', 'isPostponed'],
       keyLabels: {
@@ -33,38 +34,15 @@ var Results = React.createClass({
         'isForfeit': 'Forfait',
         'isPostponed': 'Reporté',
         'text':'match'
-      },
-      showNewForm: false
+      }
     }
   },
 
-  componentDidMount: function () {
-    api.getEntries("result").then((data) => {
-      this.setState({pages: data})
-    })
-  },
-
-  toggleNewForm: function() {
-    this.setState({ showNewForm: !this.state.showNewForm });
-  },
-
-  _onNew: function (page) {
-    var pages = this.state.pages.slice()
-    pages.unshift(page)
-    this.setState({pages: pages})
-    Router.transitionTo('result', {matchId: page._id})
-  },
-
-  _onDelete: function (id, e) {
-    if (e) {
-      e.preventDefault()
-    }
-    if (confirm('Êtes-vous sûr de vouloir supprimer ce résultat ?')) {
-      api.deleteEntry("result", id).then(() => {
-        var pages = this.state.pages.filter(page => page._id !== id)
-        this.setState({pages: pages})
-      })
-    }
+  _onNew: function (result) {
+    var results = this.state.results.slice()
+    results.unshift(result)
+    this.setState({results: results})
+    Router.transitionTo('result', {matchId: result._id})
   },
 
   goTo: function (id, e) {
@@ -75,50 +53,37 @@ var Results = React.createClass({
   },
 
   render: function () {
-    if (!this.state.pages) {
-      return <div className='pages'>Loading...</div>
+    if (!this.state.results) {
+      return <div className='results'>Loading...</div>
     }
-    var current = this.state.pages[this.state.selected] || {}
+    var current = this.state.results[this.state.selected] || {}
     var url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/')
     var rootPath = url.slice(0, url.indexOf('admin')).join('/')
     return <div className="posts">
-      <div className="posts_header">
-        <h2>Résultats</h2>
-        <button onClick={this.toggleNewForm} className="new-result-button">
-          <i className="fa fa-plus" /> {this.state.showNewForm ? 'Annuler' : 'Nouveau résultat'}
-        </button>
-      </div>
-      {this.state.showNewForm && (
-        <div className="new-result-form-container">
-          <NewResult onNew={this._onNew}/>
-        </div>
-      )}
       <ul className='posts_list'>
+        <NewResult onNew={this._onNew}/>
         {
-          this.state.pages.map((page, i) =>
-            <li key={page._id} className={cx({
-                "posts_post": false,
-                "posts_post--draft": page.isDraft,
+          this.state.results.map((result, i) =>
+            <li key={result._id} className={cx({
+                "posts_post": true,
+                "posts_post--draft": result.isDraft,
                 "posts_post--selected": i === this.state.selected
               })}
-              onDoubleClick={this.goTo.bind(null, page._id)}
+              onDoubleClick={this.goTo.bind(null, result._id)}
               onClick={this.setState.bind(this, {selected: i}, null)}
             >
               <span className="posts_post-title">
-                {page.text}
+                {result.text}
               </span>
               <span className="posts_post-date">
-                {moment(page.date).format('MMM Do YYYY')}
+                {moment(result.date).format('MMM Do YYYY')}
               </span>
-              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + page.path}>
+              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + result.path}>
                 <i className='fa fa-link'/>
               </a>
-              <Link className='posts_edit-link' to="data" matchId={page._id}>
+              <Link className='posts_edit-link' to="result" matchId={result._id}>
                 <i className='fa fa-pencil'/>
               </Link>
-              <a className='posts_delete-link' onClick={this._onDelete.bind(null, page._id)}>
-                <i className='fa fa-trash'/>
-              </a>
             </li>
           )
         }
--- hexo-admin-master/client/stades.js
+++ hexo-admin-master/client/stades.js
@@ -10,49 +10,27 @@ var Rendered = require('./rendered')
 var DataFetcher = require('./data-fetcher');
 var NewStade = require('./new-stade')
 var api = require('./api');
- 
+
 var Stades = React.createClass({
   mixins: [DataFetcher((params) => {
     return {
-     
+      stades: api.getEntries("stade").then((stades) =>
+        _.sortBy(stades, ['date']).reverse()
+      )
     }
   })],
 
   getInitialState: function () {
     return {
-      selected: 0,
-      pages: [], // Initialisation à null pour indiquer que les données ne sont pas encore chargées
-      showNewForm: false
+      selected: 0
     }
   },
 
-  componentDidMount: function () {
-    api.getEntries("stade").then((data) => {
-      this.setState({pages: data})
-    })
-  },
-
-  toggleNewForm: function() {
-    this.setState({ showNewForm: !this.state.showNewForm });
-  },
-
-  _onNew: function (page) {
-    var pages = this.state.pages.slice()
-    pages.unshift(page)
-    this.setState({pages: pages})
-    Router.transitionTo('stade', {matchId: page._id})
-  },
-
-  _onDelete: function (id, e) {
-    if (e) {
-      e.preventDefault()
-    }
-    if (confirm('Êtes-vous sûr de vouloir supprimer ce stade ?')) {
-      api.deleteEntry("stade", id).then(() => {
-        var pages = this.state.pages.filter(page => page._id !== id)
-        this.setState({pages: pages})
-      })
-    }
+  _onNew: function (stade) {
+    var stades = this.state.stades.slice()
+    stades.unshift(stade)
+    this.setState({stades: stades})
+    Router.transitionTo('stade', {matchId: stade._id})
   },
 
   goTo: function (id, e) {
@@ -63,50 +41,37 @@ var Stades = React.createClass({
   },
 
   render: function () {
-    if (!this.state.pages) {
-      return <div className='pages'>Loading...</div>
+    if (!this.state.stades) {
+      return <div className='stades'>Loading...</div>
     }
-    var current = this.state.pages[this.state.selected] || {}
+    var current = this.state.stades[this.state.selected] || {}
     var url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/')
     var rootPath = url.slice(0, url.indexOf('admin')).join('/')
     return <div className="posts">
-      <div className="posts_header">
-        <h2>Stades</h2>
-        <button onClick={this.toggleNewForm} className="new-stade-button">
-          <i className="fa fa-plus" /> {this.state.showNewForm ? 'Annuler' : 'Nouveau stade'}
-        </button>
-      </div>
-      {this.state.showNewForm && (
-        <div className="new-stade-form-container">
-          <NewStade onNew={this._onNew}/>
-        </div>
-      )}
       <ul className='posts_list'>
+        <NewStade onNew={this._onNew}/>
         {
-          this.state.pages.map((page, i) =>
-            <li key={page._id} className={cx({
-                "posts_post": false,
-                "posts_post--draft": page.isDraft,
+          this.state.stades.map((stade, i) =>
+            <li key={stade._id} className={cx({
+                "posts_post": true,
+                "posts_post--draft": stade.isDraft,
                 "posts_post--selected": i === this.state.selected
               })}
-              onDoubleClick={this.goTo.bind(null, page._id)}
+              onDoubleClick={this.goTo.bind(null, stade._id)}
               onClick={this.setState.bind(this, {selected: i}, null)}
             >
               <span className="posts_post-title">
-                {page.stadeName}
+                {stade.stadeName}
               </span>
               <span className="posts_post-date">
-                {moment(page.date).format('MMM Do YYYY')}
+                {moment(stade.date).format('MMM Do YYYY')}
               </span>
-              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + page.path}>
+              <a className='posts_perma-link' target="_blank" href={rootPath + '/' + stade.path}>
                 <i className='fa fa-link'/>
               </a>
-              <Link className='posts_edit-link' to="data" matchId={page._id}>
+              <Link className='posts_edit-link' to="stade" matchId={stade._id}>
                 <i className='fa fa-pencil'/>
               </Link>
-              <a className='posts_delete-link' onClick={this._onDelete.bind(null, page._id)}>
-                <i className='fa fa-trash'/>
-              </a>
             </li>
           )
         }
