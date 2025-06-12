720b0afd3e8e75c7ba823b9adc94b6ec2942e512
alphaleadership <thomas.iniguez@free.fr>
2025-06-12 12:41:12 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/run.js
+++ hexo-admin-master/client/run.js
@@ -2228,6 +2228,11 @@ class TournamentResult {
 
   render() {
     this.node.innerHTML = this.template();
+    const form = this.node.querySelector('form');
+    const matchSelect = form.querySelector('[name="matchId"]');
+
+    // Charger les matchs disponibles
+    this.loadMatches(matchSelect);
     this.fetchResult().then(()=>{
       this.updateView();
     })
