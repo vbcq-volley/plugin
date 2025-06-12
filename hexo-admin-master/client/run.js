72751dd475b83cbf073ba60eb314c8a9c1255890
alphaleadership <thomas.iniguez@free.fr>
2025-06-12 10:04:59 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/run.js
+++ hexo-admin-master/client/run.js
@@ -2097,7 +2097,9 @@ class TournamentMatch {
 
     // Remplir les options des équipes
     const allTeams = [...this.teams, ...this.tournamentTeams];
-    if(t)
+    if(this.data){
+      
+    }
     const teamOptions = allTeams.map(team => 
       `<option value="${team.id}" ${this.data.team1 === team.id ? 'selected' : ''}>${team.name}</option>`
     ).join('');
