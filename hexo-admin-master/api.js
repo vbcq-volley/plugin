f7aa547f15fa435539b235c18f1b20a1dc2411dd
alphaleadership <thomas.iniguez@free.fr>
2025-06-17 08:57:04 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/api.js
+++ hexo-admin-master/api.js
@@ -692,7 +692,7 @@ return res.done(db.read(req.body.data.type))  }
     }
 
     var postParameters = {title: req.body.title, layout: 'posts', date: new Date(), author: hexo.config.author,source:"_post"};
-    hexo.log.d(postParameters)
+    hexo.log(postParameters)
     hexo.post.create(postParameters)
     .error(function(err) {
       hexo.log.d("l'erreur est a la création du post")
@@ -701,8 +701,8 @@ return res.done(db.read(req.body.data.type))  }
     })
     .then(function (file) {
       var source = postParameters.source
-      hexo.log.d(file)
-      res.send(200,"succes")
+      hexo.log(file)
+      res.done(addIsDraft(file))
     });
   });
 
