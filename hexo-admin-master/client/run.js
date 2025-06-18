4515e9abde974f0cc293888c20039b204354e6b3
alphaleadership <thomas.iniguez@free.fr>
2025-06-18 16:53:47 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/run.js
+++ hexo-admin-master/client/run.js
@@ -2283,6 +2283,7 @@ class TournamentGenerator {
       // Générer les matchs selon le type de tournoi
       if(type=="poule"){
         const groups = this.divideTeamsIntoGroups(teams);
+        groups.f
       }
 
       // Créer les matchs via l'API
