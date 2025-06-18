ab8422f7046a7cb9019c92e24601cc8abf54f56d
alphaleadership <thomas.iniguez@free.fr>
2025-06-18 15:22:21 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/client/run.js
+++ hexo-admin-master/client/run.js
@@ -2085,7 +2085,7 @@ class TournamentGenerator {
   }
 
   // Fonction utilitaire pour fermer la popup
-  static closePopup(popup) {
+   closePopup(popup) {
     popup.style.display = 'none';
     document.body.style.overflow = 'auto';
   }
@@ -2210,21 +2210,21 @@ class TournamentGenerator {
         const selectedType = Array.from(typeButtons).find(btn => btn.classList.contains('selected'))?.dataset.type;
         if (selectedType && dateInput.value) {
           resolve({ type: selectedType, startDate: dateInput.value });
-          closePopup(popup);
+          this.closePopup(popup);
         }
       });
 
       // Gérer l'annulation
       cancelButton.addEventListener('click', () => {
         resolve(null);
-        closePopup(popup);
+          this.closePopup(popup);
       });
 
       // Fermer la popup en cliquant en dehors
       popup.addEventListener('click', (e) => {
         if (e.target === popup) {
           resolve(null);
-          closePopup(popup);
+          this.closePopup(popup);
         }
       });
     });
