5346e8aaa058b8eb2f8f4bb1bd0187efd48ed7c3
alphaleadership <thomas.iniguez@free.fr>
2025-06-19 09:54:27 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-master/api.js
+++ hexo-admin-master/api.js
@@ -678,7 +678,7 @@ function generateTournamentMatches(type, startDate, teams) {
         if(currentRound === 'quart'){
           team1Ref = teams[i]._id;
           team2Ref = teams[i + 1]._id;
-        }else{
+        }else{console.log(matches.find(m => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team1 === m._id||match.team2 === m._id)))
           team1Ref = matches.find(m => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team1 === m._id||match.team2 === m._id)).winner;
           team2Ref = matches.find(m => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team2 === m._id||match.team1 === m._id)).winner;
         }
